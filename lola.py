79e1c03cf380639498393d13a5b2aab0ca4f3a06
lolaa <lolabena06@gmail.com>
2024-06-29 14:48:16 -0300

    modificacion 2

--- lola.py
+++ lola.py
@@ -4,12 +4,34 @@ contraseña= input("cree una contraseña: ")
 
 ingresar= input("desea ingresar? si/no: ")
 
-if ingresar=="si":
-    clave=input("ingrese su contraseña: ")
-    if clave==contraseña:
-        print("se ingreso correctamente")
-    else:
-        print ("la contraseña es incorrecta")
+while True:
+    if ingresar=="si":
+        clave=input("ingrese su contraseña: ")
+        if clave==contraseña:
+            print("se ingreso correctamente")
+            print ("bienvenido a calculadora basica")
+            primer_num= int(input("ingrese primer numero: "))
+            operacion= input ("desea sumar, restar, multiplicar, dividir: ")
+            segundo_num= int(input("ingrese segundo numero: "))
 
-else:
-    print("cerrando sitio")
+            if operacion=="sumar":
+                resolucion= primer_num + segundo_num
+            elif operacion== "restar":
+                resolucion= primer_num - segundo_num
+            elif operacion== "multiplicar":
+                resolucion= primer_num * segundo_num
+            elif operacion== "dividir":
+                resolucion= primer_num/segundo_num
+            else: 
+                print("ingrese operación válida")
+            
+            print("el resultado es:", resolucion)
+
+        else:
+            print ("la contraseña es incorrecta")
+
+    else:
+        print("cerrando sitio")
+        break
+    
+    
